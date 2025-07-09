994b7d6a61e09dd7da3c6ff2c4e1fab89c16289a
sunz8281 <sunz8281@gmail.com>
2025-07-09 18:02:32 +0900

    refactor(#2): format & lint 적용

--- src/components/common/NavSidebar/NavSidebar.tsx
+++ src/components/common/NavSidebar/NavSidebar.tsx
@@ -1,25 +1,26 @@
-import styled from '@emotion/styled'
-import { color } from '@odyssey-horizon/ui'
-import { Column, HorizonLogo, Row, Text } from '@odyssey-horizon/ui'
-import Link from 'next/link'
-import { ETC_ITEMS, MAIN_ITEMS } from './NavSidebar.constants.ts'
-import { SidebarItem } from './SidebarItem/SidebarItem.tsx'
+'use client';
+
+import styled from '@emotion/styled';
+import { color, Column, HorizonLogo, Row, Text } from '@odyssey-horizon/ui';
+import Link from 'next/link';
+import { ETC_ITEMS, MAIN_ITEMS } from './NavSidebar.constants';
+import { SidebarItem } from './SidebarItem/SidebarItem';
 
 const NavSidebar = () => {
   return (
     <StyledSidebar>
       <LogoBox>
-        <Row width="226px">
-          <Link href="/">
-            <HorizonLogo height="40px" />
+        <Row width='226px'>
+          <Link href='/'>
+            <HorizonLogo height='40px' />
           </Link>
         </Row>
       </LogoBox>
-      <Column gap="10px" padding="10px 12px">
-        <Text variant="B_M_12" color={color.grayscale['400']} style={{ marginLeft: '8px' }}>
+      <Column gap='10px' padding='10px 12px'>
+        <Text variant='B_M_12' color={color.grayscale['400']} style={{ marginLeft: '8px' }}>
           메인
         </Text>
-        <Column gap="10px">
+        <Column gap='10px'>
           {MAIN_ITEMS.map(({ name, icon, to }) => (
             <SidebarItem key={to} icon={icon} to={to}>
               {name}
@@ -27,14 +28,14 @@ const NavSidebar = () => {
           ))}
         </Column>
       </Column>
-      <Row height="10px" alignItems="center">
+      <Row height='10px' alignItems='center'>
         <Divider />
       </Row>
-      <Column gap="10px" padding="10px 12px">
-        <Text variant="B_M_12" color={color.grayscale['400']} style={{ marginLeft: '8px' }}>
+      <Column gap='10px' padding='10px 12px'>
+        <Text variant='B_M_12' color={color.grayscale['400']} style={{ marginLeft: '8px' }}>
           기타
         </Text>
-        <Column gap="10px">
+        <Column gap='10px'>
           {ETC_ITEMS.map(({ name, icon, to }) => (
             <SidebarItem key={to} icon={icon} to={to}>
               {name}
@@ -43,10 +44,10 @@ const NavSidebar = () => {
         </Column>
       </Column>
     </StyledSidebar>
-  )
-}
+  );
+};
 
-export default NavSidebar
+export default NavSidebar;
 
 const StyledSidebar = styled.div`
   width: 266px;
@@ -54,7 +55,7 @@ const StyledSidebar = styled.div`
   background: ${color.white};
   border-right: 1px solid ${color.grayscale['100']};
   box-sizing: border-box;
-`
+`;
 
 const LogoBox = styled.div`
   width: 100%;
@@ -63,9 +64,9 @@ const LogoBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const Divider = styled.div`
   border-bottom: 1px solid ${color.grayscale['100']};
   width: 100%;
-`
+`;
--- src/components/common/NavSidebar/SidebarItem/SidebarItem.tsx
+++ src/components/common/NavSidebar/SidebarItem/SidebarItem.tsx
@@ -1,42 +1,47 @@
-import styled from '@emotion/styled'
-import { color } from '@odyssey-horizon/ui'
-import { Icon, Row, Text } from '@odyssey-horizon/ui'
-import { ReactNode } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+'use client';
+
+import styled from '@emotion/styled';
+import { color } from '@odyssey-horizon/ui';
+import { Icon, Row, Text } from '@odyssey-horizon/ui';
+import { ReactNode } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface SidebarItemProps {
-  children: ReactNode
-  icon: string
-  to: string
+  children: ReactNode;
+  icon: string;
+  to: string;
 }
 
 export const SidebarItem = ({ children, icon, to }: SidebarItemProps) => {
-  const location = useLocation()
-  const isActive = location.pathname === to
+  const pathname = usePathname();
+  const isActive = pathname === to;
 
   return (
-    <StyledSidebarItem to={to} selected={isActive}>
-      <Row gap="8px" alignItems="center">
-        <Icon
-          name={icon}
-          variant="Stroke_L_24"
-          style={{
-            color: isActive ? color.primary['500'] : color.grayscale['600'],
-          }}
-        />
-        <Text variant="B_M_14" color={isActive ? color.primary['500'] : color.grayscale['600']}>
-          {children}
-        </Text>
-      </Row>
-    </StyledSidebarItem>
-  )
-}
+    <Link href={to}>
+      <StyledSidebarItem selected={isActive}>
+        <Row gap='8px' alignItems='center'>
+          <Icon
+            name={icon}
+            variant='Stroke_L_24'
+            style={{
+              color: isActive ? color.primary['500'] : color.grayscale['600'],
+            }}
+          />
+          <Text variant='B_M_14' color={isActive ? color.primary['500'] : color.grayscale['600']}>
+            {children}
+          </Text>
+        </Row>
+      </StyledSidebarItem>
+    </Link>
+  );
+};
 
 interface StyledSidebarItemProps {
-  selected: boolean
+  selected: boolean;
 }
 
-const StyledSidebarItem = styled(Link)<StyledSidebarItemProps>`
+const StyledSidebarItem = styled.div<StyledSidebarItemProps>`
   width: 100%;
   padding: 8px;
   border-radius: 8px;
@@ -47,4 +52,4 @@ const StyledSidebarItem = styled(Link)<StyledSidebarItemProps>`
     background-color: ${({ selected }) =>
       selected ? color.primary['100'] : color.grayscale['100']};
   }
-`
+`;
