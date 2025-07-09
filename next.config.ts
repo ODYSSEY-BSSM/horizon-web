13df5355b81ed343235aed4badbf6242ab692df4
sunz8281 <sunz8281@gmail.com>
2025-07-09 18:04:55 +0900

    feat(#2): globalStyle 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    transpilePackages: ['@odyssey-horizon/ui'],
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@odyssey-horizon/ui': require.resolve('@odyssey-horizon/ui'),
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
---
+++ src/components/common/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Global } from '@emotion/react';
+import { global } from '@odyssey-horizon/ui';
+
+export default function GlobalStyle() {
+    return <Global styles={global} />;
+}
