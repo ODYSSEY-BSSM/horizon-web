afe781757487b257f413200043ce3c27000604a8
sunz8281 <sunz8281@gmail.com>
2025-07-10 13:29:48 +0900

    feat(#8): toggle 컴포넌트 제작

---
+++ src/components/dashboard/Toggle/Toggle.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+import {Icon, Text, color } from "@odyssey-horizon/ui";
+import React from "react";
+
+interface ToggleProps {
+    children: React.ReactNode;
+    title?: string;
+}
+
+const Toggle = ({children, title}: ToggleProps) => {
+    const [isOpen, setIsOpen] = React.useState(true);
+
+    return (
+        <StyledToggle>
+            <TitleBox onClick={() => setIsOpen((prev) => !prev)}>
+                <Text variant='H_B_16' color={isOpen? color.black:color.grayscale['400']}>{title}</Text>
+                <Icon name={isOpen? 'arrow_drop_down':'arrow_drop_up'} variant='Stroke_L_24' style={{ color: isOpen? color.black:color.grayscale['400']}}/>
+            </TitleBox>
+            {isOpen && children}
+        </StyledToggle>
+    );
+}
+
+export default Toggle;
+
+const StyledToggle = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+`
+
+const TitleBox = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    cursor: pointer;
+`
