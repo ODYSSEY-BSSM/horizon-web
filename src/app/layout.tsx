608dc31923228cc3b6325196965cf3efbe3ae162
sunz8281 <sunz8281@gmail.com>
2025-07-15 15:18:02 +0900

    feat(#10): QueryClientProvider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import GlobalStyle from '@/components/common/GlobalStyle/GlobalStyle.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang='en'>
       <body>
-        <GlobalStyle />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
